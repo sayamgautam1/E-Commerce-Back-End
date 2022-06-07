f4846f3eb550010f73eafe2b2a032e17d0673f6f
Sayam Gautam <sayamgautam1@gmail.com>
2022-06-07 14:04:31 +0930

    add relation bwtween product, tag and categorey

--- models/index.js
+++ models/index.js
@@ -1,16 +1,39 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
 
+Product.belongsTo(Category, {
+  foreignKey: category_id,
+});
+
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: category_id,
+  onDelete: "CASCADE",
+});
 
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    foreignKey: product_id,
+  },
+  //define an alias for when data is recieved
+  as: "products",
+});
 // Tags belongToMany Products (through ProductTag)
+
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    foreignKey: tag_id,
+  },
+  as: "tags",
+});
 
 module.exports = {
   Product,
