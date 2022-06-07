04b7bb30ec2d166f0878c945bc943635b397a24c
Sayam Gautam <sayamgautam1@gmail.com>
2022-06-07 15:13:30 +0930

    add put route to upadate category by it's id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -45,6 +45,19 @@ router.post("/", async (req, res) => {
 
 router.put("/:id", async (req, res) => {
   // update a category by its `id` value
+  try {
+    const categoryData = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: "no matching id" });
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.delete("/:id", (req, res) => {
